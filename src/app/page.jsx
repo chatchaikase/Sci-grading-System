9c18232fce353c3a0b6f19ac6ac114642c2ca191
Pongpan NERUNCHORN <pongpan.ne@live.ku.th>
2023-12-04 19:45:45 +0700

    Create Nav bar responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-   <main>
-    test
-  </main>
-  )
+    <>
+      <span className="font-bold text-4xl">Home</span>
+      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
+    </>
+  );
 }

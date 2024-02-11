db31ad778378408578697e11601ba6cc92a95d40
Travea <chanchai.e@ku.th>
2024-02-11 21:07:32 +0700

    เปลี่ยนเซ็ทติ้งเพจให้เป็น10

--- src/components/Pageination/Pageination.jsx
+++ src/components/Pageination/Pageination.jsx
@@ -10,7 +10,7 @@ export default function Pagination({ count }) {
   const page = searchParams.get("page") || 1;
 
   const params = new URLSearchParams(searchParams);
-  const ITEM_PER_PAGE = 8;
+  const ITEM_PER_PAGE = 10;
 
   const hasPrev = ITEM_PER_PAGE * (parseInt(page) - 1) > 0;
   const hasNext = ITEM_PER_PAGE * (parseInt(page) - 1) + ITEM_PER_PAGE < count;

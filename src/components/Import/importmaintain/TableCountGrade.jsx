bbeb9543c7f465e2aae6fefe4083f994aca8e448
iampetch <ratchanon788@gmail.com>
2024-02-15 11:41:20 +0700

    เปลี่ยนรูปเเบบ count Grade

--- src/components/Import/importmaintain/TableCountGrade.jsx
+++ src/components/Import/importmaintain/TableCountGrade.jsx
@@ -14,7 +14,7 @@ export default function TableCountGrade({ excelData, loading }) {
     });
 
     return gradeCount;
-  }
+  };
 
   const gradeCount = countGrades(excelData ?? null);
 
@@ -23,47 +23,59 @@ export default function TableCountGrade({ excelData, loading }) {
   } else {
     const sortedGrades = ["A", "B+", "B", "C+", "C", "D+", "D"];
     const gradeColorMap = {
-      "A": "green-600",
-      "B": "primary",
+      A: "green-600",
+      B: "primary",
       "B+": "blue-600",
-      "C": "yellow-600",
+      C: "yellow-600",
       "C+": "orange-500",
       "D+": "red-500",
-      "D": "secondary",
+      D: "primary-content",
     };
     const totalStudents = excelData.length;
 
     return (
-      <div className="">
-        {excelData && (
-          <div>
-            {/* แสดงจำนวนนักศึกษาที่ได้แต่ละเกรด */}
-            <div className="mt-5 flex flex-col w-[cal(1200px-200px)] p-3 gap-2 items-end justify-center">
-              {sortedGrades.map((grade) => (
-                <button
-                  className={`btn bg-${gradeColorMap[grade]} text-white w-[250px]`}
-                  key={grade}
-                >
-                  {grade}
-                  <div
-                    className={`badge badge-white text-${gradeColorMap[grade]}`}
+      <div className="flex justify-end">
+        {!loading && excelData ? (
+          <div className="table-responsive mt-5 border border-solid w-[30%] max-h-[800px] overflow-y-auto">
+            <table className="w-full">
+              <thead className="bg-gray-50 border-b-2 border-gray-200">
+                <tr>
+                  <th
+                    className="text-center w-[60%] p-3 text-lg font-semibold tracking-wide"
                   >
-                    {gradeCount[grade] || 0} คน
-                  </div>
-                </button>
-              ))}
-              <button
-                className={`btn bg-slate-700 text-white w-[250px]`}
-              >
-                จำนวนทั้งหมด
-                <div
-                  className={`badge badge-white text-slate-700`}
-                >
-                  {totalStudents} คน
-                </div>
-              </button>
-            </div>
+                    เกรด
+                  </th>
+                  <th
+                    className="text-center w-[40%] p-3 text-lg font-semibold tracking-wide"
+                  >
+                    คน
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-100">
+                {sortedGrades.map((grade) => (
+                  <tr className="bg-white" key={grade}>
+                    <td className={`text-center p-3 text-lg text-gray-700 whitespace-nowrap`}>
+                      {grade}
+                    </td>
+                    <td className={`text-center p-3 text-lg text-gray-700 whitespace-nowrap`}>
+                      {gradeCount[grade] || 0}
+                    </td>
+                  </tr>
+                ))}
+                <tr className="bg-white">
+                  <td className={`text-center p-3 text-lg text-gray-700 whitespace-nowrap`}>
+                    จำนวนทั้งหมด
+                  </td>
+                  <td className={`text-center p-3 text-lg text-gray-700 whitespace-nowrap`}>
+                    {totalStudents}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
+        ) : (
+          <></>
         )}
       </div>
     );

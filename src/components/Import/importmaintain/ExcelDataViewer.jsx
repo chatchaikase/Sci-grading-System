f99dee06b5f03a91602e1b1980e692d05ccf7723
iampetch <ratchanon788@gmail.com>
2024-02-16 13:08:29 +0700

    อัปเดตการสกอร์ในตาราง

--- src/components/Import/importmaintain/ExcelDataViewer.jsx
+++ src/components/Import/importmaintain/ExcelDataViewer.jsx
@@ -4,13 +4,13 @@ export default function ExcelDataViewer({ excelData, loading }) {
   return (
     <div className="viewer ">
       {!loading && excelData ? (
-        <div className="table-responsive mt-5 border border-solid max-h-[800px] overflow-y-auto">
+        <div className="table-container table-responsive mt-5 border border-solid max-h-[800px] overflow-y-auto">
           <table className="w-full">
             <thead className="bg-gray-50 border-b-2 border-gray-200">
               <tr>
                 {Object.keys(excelData[0]).map((key) => (
                   <th
-                    className="text-center w-10 p-3 text-lg font-semibold tracking-wide"
+                    className="text-center w-10 p-3 text-lg font-semibold tracking-wide sticky top-0 bg-white"
                     key={key}
                   >
                     {key}
@@ -23,7 +23,7 @@ export default function ExcelDataViewer({ excelData, loading }) {
                 <tr className="bg-white" key={index}>
                   {Object.keys(individualExcelData).map((key) => (
                     <td
-                      className="text-center p-3 text-lg text-gray-700 whitespace-nowrap "
+                      className="text-center p-3 text-lg text-gray-700 whitespace-nowrap"
                       key={key}
                     >
                       {key.toLowerCase().includes("date") &&

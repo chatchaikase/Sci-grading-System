9849b5b364b3aa425cd9484d21a9ec2836f60162
iampetch <121572068+iampetch@users.noreply.github.com>
2024-02-20 23:03:24 +0700

    อัปเดต package

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google'
-import { Noto_Sans_Thai } from '@next/font/google'
+import { Noto_Sans_Thai } from "next/font/google"
 import './globals.css'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';

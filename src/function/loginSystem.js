ad864757c418693eeabdcce95fce1ae4479c1015
BBT0423 <pongpan22120@gmail.com>
2024-03-26 03:16:21 +0700

    Update loginSystem.js

--- src/function/loginSystem.js
+++ src/function/loginSystem.js
@@ -14,6 +14,7 @@ export const login = async(previousState,formData)=>{
     
     try {
         await signIn("credentials",{username,password})
+        console.log("is login");
         return { success:true}
     } catch (error) {
         if(!error.message.includes("NEXT_REDIRECT")){

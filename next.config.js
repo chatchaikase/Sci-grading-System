666a74da4025628a7082e9f746272b4dc32da6ef
BBT0423 <pongpan22120@gmail.com>
2024-03-26 01:27:00 +0700

    add export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: process.env.NEXT_OUTPUT_MODE,
+  exclude: ["api", "middleware"],
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
